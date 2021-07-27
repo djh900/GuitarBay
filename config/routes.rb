462edce8a1672d12096b894327fedab2c132765c
Daniel Hinton <djhinton96@outlook.com>
2021-07-27 14:47:33 +1000

    Create routes for pages and listings controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'index', to: 'pages#home'
+  get 'help', to: 'pages#help'
+  resources :listings
 end
