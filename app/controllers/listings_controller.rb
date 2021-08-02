4c34cf27d0bdbcf1e56f83a5e9016f503703c7b7
Daniel Hinton <djhinton96@outlook.com>
2021-08-02 14:54:07 +1000

    Fill index and show method bodies

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,19 @@
 class ListingsController < ApplicationController
-  def index; end
+  def index
+    @listings = Listing.all
+  end
   
   def new; end
 
   def create; end
 
-  def show; end
+  def show
+    begin
+      @listing = Listing.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render_404
+    end
+  end
 
   def edit; end
 
