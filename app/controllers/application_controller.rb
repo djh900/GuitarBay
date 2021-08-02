8f7cad3a4e6911ea5ed86354c90903c89ee060c9
Daniel Hinton <djhinton96@outlook.com>
2021-08-02 14:53:56 +1000

    Add code to allow redirection to 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # These two methods help the application redirect to the 404 page when the listing ID in params doesn't exist in the database
+  # Code used: https://gist.github.com/AhmedNadar/b450fd65eda9c4afb8e04e28f1348af6
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  rescue
+    render_404
+  end
+
+  def render_404
+    render file: "#{Rails.root}/public/404.html", status: :not_found
+  end
+  
   protected
 
   def configure_permitted_parameters
