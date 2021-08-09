d442496c675ac660e6e25fe4753f815c720a302f
Daniel Hinton <djhinton96@outlook.com>
2021-08-09 18:04:38 +1000

    Fill create method

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,21 +6,20 @@ class ListingsController < ApplicationController
     @listings = Listing.all
   end
   
-  
   def new
-    @categories = Category.all
-    @manufacturers = Manufacturer.all
-    @conditions = Condition.all
-    @colors = Color.all
-    @materials = Material.all
-    @fingerboards = Fingerboard.all
-    @bridges = Bridge.all
-    @countries = Country.all
-    @cases = Case.all
-    @deliveries = Delivery.all
+    prefill_select_menus
+    @listing = Listing.new
   end
 
-  def create; end
+  def create
+    @listing = Listing.new(strong_params)
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      prefill_select_menus
+      render 'new'
+    end
+  end
 
   def show
     begin
@@ -35,5 +34,24 @@ class ListingsController < ApplicationController
   def update; end
 
   def destroy; end
+
+  private
+
+  def strong_params
+    params.require(:listing).permit(:category_id, :manufacturer_id, :model, :condition_id, :price, :color_id, :material_id, :fingerboard_id, :scale_length, :fingerboard_radius, :number_of_frets, :bridge_id, :neck_pickup, :bridge_pickup, :country_id, :year_manufactured, :serial_number, :case_id, :description, :delivery_id)
+  end
+
+  def prefill_select_menus
+    @categories = Category.all
+    @manufacturers = Manufacturer.all
+    @conditions = Condition.all
+    @colors = Color.all
+    @materials = Material.all
+    @fingerboards = Fingerboard.all
+    @bridges = Bridge.all
+    @countries = Country.all
+    @cases = Case.all
+    @deliveries = Delivery.all
+  end
 
 end
