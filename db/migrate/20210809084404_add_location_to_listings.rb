c0f7a30710413e014c67a2940957d2c98845bca0
Daniel Hinton <djhinton96@outlook.com>
2021-08-09 18:47:40 +1000

    Add location attribute to listing model

---
+++ db/migrate/20210809084404_add_location_to_listings.rb
@@ -0,0 +1,5 @@
+class AddLocationToListings < ActiveRecord::Migration[6.1]
+  def change
+    add_column :listings, :location, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_043336) do
+ActiveRecord::Schema.define(version: 2021_08_09_084404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2021_08_02_043336) do
     t.boolean "sold"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "location"
     t.index ["bridge_id"], name: "index_listings_on_bridge_id"
     t.index ["case_id"], name: "index_listings_on_case_id"
     t.index ["category_id"], name: "index_listings_on_category_id"
