68af12210fb8eb2e7332f1f68f386dcc0e14f053
Daniel Hinton <djhinton96@outlook.com>
2021-08-10 21:06:49 +1000

    Pass listing ID to success method

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -51,7 +51,7 @@ class ListingsController < ApplicationController
           listing_id: @listing.id
         } 
       },
-      success_url: "#{root_url}/success?year=#{@listing.year_manufactured}&manufacturer=#{@listing.manufacturer.name}&model=#{@listing.model}",
+      success_url: "#{root_url}/success?year=#{@listing.year_manufactured}&manufacturer=#{@listing.manufacturer.name}&model=#{@listing.model}&id=#{@listing.id}",
       cancel_url: "#{root_url}/listings/#{@listing.id}"
     )
 
