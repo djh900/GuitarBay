93f6e4c91a5862d6add4a5265ece80bb95060f74
Daniel Hinton <djhinton96@outlook.com>
2021-08-12 19:01:22 +1000

    Sanitize username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_save :enforce_lower_case_username
+
+  def enforce_lower_case_username
+    self.username = self.username.downcase
+  end
 end
